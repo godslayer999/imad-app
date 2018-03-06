2a30b0d63aa2606dc5f5f3839ada2fb09aa780a0
godslayer999 <36673450+godslayer999@users.noreply.github.com>
2018-03-06 19:52:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element =document.getElementById("main-text");
 element.innerHTML="New Value";
+//Move the image
+
+var img= document.getElementById("main-text");
+img.onclick=function(){
+    img.style.marginLeft="100px";
+}
