2bc2c7fbb3417395b3d201345a3d5d0196105f66
godslayer999 <36673450+godslayer999@users.noreply.github.com>
2018-03-06 19:53:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element =document.getElementById("main-text");
 element.innerHTML="New Value";
 //Move the image
 
-var img= document.getElementById("main-text");
+var img= document.getElementById("madi");
 img.onclick=function(){
     img.style.marginLeft="100px";
 }
