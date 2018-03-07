30d0e8ca1dcfc55dcd7fd8d7186039219accf610
godslayer999 <36673450+godslayer999@users.noreply.github.com>
2018-03-07 15:49:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,32 @@ if(request.readyState===XMLHttpRequest.DONE){
 request.open('GET','http://vishalvishnu9061.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+
+
+var submit =document.getElementById('submit_btn');
+submit.onclick=function(){
+//create a request object 
+var request = new XMLHttpRequest();
+
+//Capture the request and store it in the variable
+request.onreadystatechange=function(){
+if(request.readyState===XMLHttpRequest.DONE){
+  //Take some action
+  if(request.status===200){
+
+var names={'name1','name2','name3','name4'};
+var list='';
+for(var i=0;i<names[i].length;i++){
+    list+='<li>'+name+'</li>';
+}
+var ul=document.getElementId('namelist');
+ul.innerHTML=list;
+
+}}
+//Not done yet
+};
+//Make the request
+request.open('GET','http://vishalvishnu9061.imad.hasura-app.io/counter',true);
+request.send(null);
+};
