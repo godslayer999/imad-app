6f42c61387ab3e331a5647c3caac34a91f43caf7
godslayer999 <36673450+godslayer999@users.noreply.github.com>
2018-03-07 16:33:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,7 @@ var list='';
 for(var i=0;i<names.length;i++){
     list+='<li>'+names[i]+'</li>';
 }
-var ul=document.getElementId('namelist');
+var ul=document.getElementById('namelist');
 ul.innerHTML=list;
 
 }}
