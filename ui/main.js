6600bea5d6b2c96dacf3ebee3de726244f2dceda
godslayer999 <36673450+godslayer999@users.noreply.github.com>
2018-04-01 17:21:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ request.send(null);
 
 
 var click2=getElementById('exp1');
-click2.onclick=function(){
+click2.onkeydown=function(){
     click2.innerHTML='I previously studied at Army Public School';
 }
 
